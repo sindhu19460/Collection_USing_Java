9bffe1731712e67e8fa7f72ef82288d68dc03904
Sindhu D <57280514+sindhu19460@users.noreply.github.com>
2024-05-29 15:24:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class Array {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add("Ramu");
+		al.add(5.8f);
+		al.add(false);
+		System.out.println(al);
+	}
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,11 @@
+package Collection;
+import java.util.*;
+public class LinkedList {
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.add(10);
+		
+	}
+
+}
